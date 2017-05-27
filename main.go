760a2f080cd8d78b535908363d7bf79da2053432
WILLIAM THING <wthing@u.washington.edu>
2017-05-26 23:57:43 -0700

    refactoring database info into db class

--- main.go
+++ main.go
@@ -5,16 +5,12 @@ package main
 
 import (
 	"bookstore/models"
-	"bufio"
-	"bytes"
 	"fmt"
 	"log"
-	"os"
 )
 
 func main() {
-	dbInfo := getDatabaseInfo()
-	models.InitDB(dbInfo)
+	models.InitDB()
 
 	books, err := models.AllBooks()
 	if err != nil {
@@ -24,36 +20,4 @@ func main() {
 	for _, bk := range books {
 		fmt.Printf("%s, %s, %s, $%.2f\n", bk.Isbn, bk.Title, bk.Author, bk.Price)
 	}
-}
-
-//	returns database connection information required: username, password,
-//	and database endpoint
-func getDatabaseInfo() string {
-	var dbInfo []string
-	var buffer bytes.Buffer
-
-	file, err := os.Open("dbinfo.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		info := scanner.Text()
-		dbInfo = append(dbInfo, info)
-	}
-
-	user := dbInfo[0]
-	pw := dbInfo[1]
-	dbConnection := dbInfo[2]
-
-	buffer.WriteString(user)
-	buffer.WriteString(":")
-	buffer.WriteString(pw)
-	buffer.WriteString("@tcp(")
-	buffer.WriteString(dbConnection)
-	buffer.WriteString(":3306)/bookstore")
-	param := buffer.String()
-	return param
 }
--- models/db.go
+++ models/db.go
@@ -1,8 +1,11 @@
 package models
 
 import (
+	"bufio"
+	"bytes"
 	"database/sql"
 	"log"
+	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 )
@@ -10,7 +13,8 @@ import (
 var db *sql.DB
 
 // InitDB opens connections to mysql database given dbInfo
-func InitDB(dbInfo string) {
+func InitDB() {
+	dbInfo := getDatabaseInfo()
 	var err error
 	db, err = sql.Open("mysql", dbInfo)
 
@@ -21,4 +25,36 @@ func InitDB(dbInfo string) {
 	if err = db.Ping(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+//	returns database connection information required: username, password,
+//	and database endpoint
+func getDatabaseInfo() string {
+	var dbInfo []string
+	var buffer bytes.Buffer
+
+	file, err := os.Open("models/dbinfo.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		info := scanner.Text()
+		dbInfo = append(dbInfo, info)
+	}
+
+	user := dbInfo[0]
+	pw := dbInfo[1]
+	dbConnection := dbInfo[2]
+
+	buffer.WriteString(user)
+	buffer.WriteString(":")
+	buffer.WriteString(pw)
+	buffer.WriteString("@tcp(")
+	buffer.WriteString(dbConnection)
+	buffer.WriteString(":3306)/bookstore")
+	param := buffer.String()
+	return param
 }
